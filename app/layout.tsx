c6a57e612b87807aa33bf907f9efd8d0d19f4741
Arnel Francia <nelffff@gmail.com>
2024-11-24 14:07:59 +0800

    updated added Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Header />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
