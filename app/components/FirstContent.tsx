179b7c66fbef132e73f1d7a331bd7e3dd5b967b3
Arnel Francia <nelffff@gmail.com>
2024-11-25 19:36:07 +0800

    updated

--- app/components/FirstContent.tsx
+++ app/components/FirstContent.tsx
@@ -1,11 +1,41 @@
+"use client";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { MdNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function FirstContent() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "/kitchen-pics/k1.jpg",
+    "/kitchen-pics/k2.jpg",
+    "/kitchen-pics/k3.jpg",
+    "/kitchen-pics/k4.jpg",
+    "/kitchen-pics/k5.jpg",
+    "/kitchen-pics/k6.jpg",
+    "/kitchen-pics/k7.jpg",
+    "/kitchen-pics/k8.jpg",
+    "/kitchen-pics/k9.jpg",
+  ];
+
+  function prevSlide() {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  }
+
+  function nextSlide() {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  }
+
   return (
     <div>
-      <div className="bg-black w-full h-96">
+      <div className="bg-black w-full h-full pb-10">
         {/*Socials*/}
         <div className="flex justify-center items-center">
           <div
@@ -24,6 +54,33 @@ export default function FirstContent() {
           </div>
         </div>
         {/*Socials*/}
+
+        <div className="relative flex items-center justify-center mt-10 px-8">
+          <div className="w-[400px] h-[300px] overflow-hidden flex items-center justify-center">
+            <Image
+              src={images[currentIndex]}
+              alt={`Slide ${currentIndex}`}
+              width={400}
+              height={300}
+              objectFit="cover"
+              className="w-full h-full  rounded"
+            />
+          </div>
+
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2"
+          >
+            <GrFormPrevious />
+          </button>
+
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2"
+          >
+            <MdNavigateNext />
+          </button>
+        </div>
       </div>
     </div>
   );
