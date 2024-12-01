d454201ec75e4f99da1ff6744b7e606c0cf9b357
Arnel Francia <nelffff@gmail.com>
2024-12-01 18:02:41 +0800

    updated

--- app/components/FirstContent.tsx
+++ app/components/FirstContent.tsx
@@ -52,7 +52,7 @@ export default function FirstContent() {
 
   return (
     <div>
-      <div className="bg-black flex flex-col justify-center items-center bg-opacity-90 w-full h-full pb-10 md:flex-row md:bg-white md:h-1200 md:ml-20 md:p-0 md:w-full">
+      <div className=" bg-black flex flex-col justify-center items-center bg-opacity-90 w-full h-full pb-10 md:flex-row md:bg-white md:h-1200">
         {/*Socials*/}
         <div className="flex justify-center items-center md:bg-black md:bg-opacity-90 md:h-4/5 md:w-10">
           <div className="flex gap-5 justify-center items-center w-full h-14 mx-8 rounded bg-charcoal md:flex-col">
@@ -69,8 +69,8 @@ export default function FirstContent() {
         </div>
         {/*Socials*/}
 
-        <div className="relative flex items-center justify-center mt-10 px-8">
-          <div className="w-[500px] h-[330px] overflow-hidden flex items-center justify-center relative rounded-3xl md:w-[450px] md:h-[600px] md:overflow-hidden">
+        <div className="relative flex items-center justify-center mt-10 px-8 md:px-0 md:h-full md:w-full">
+          <div className="w-[500px] h-[330px] overflow-hidden flex items-center justify-center relative rounded-3xl md:w-[450px] md:h-[600px] md:overflow-hidden md:z-10">
             {/* Previous Image */}
             {prevIndex !== null && (
               <Image
@@ -105,17 +105,16 @@ export default function FirstContent() {
 
             <button
               onClick={nextSlide}
-              className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2 "
+              className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white rounded-full p-2"
               aria-label="Next Slide"
             >
               <MdNavigateNext />
             </button>
           </div>
-        </div>
 
-        <div className="flex flex-col justify-center items-center gap-6 mt-8 px-10 md:bg-black md:h-full">
+          <div className="flex flex-col justify-center items-center gap-6 mt-8 px-10 md:bg-black md:h-full md:absolute md:right-0 md:z-0">
           <div className="w-56">
-            <h1 className="text-white text-5xl text-justify font-extrabold font-jost ">
+            <h1 className="text-white text-5xl text-justify font-extrabold font-jost  md:h-full">
               LUXURY MODERN KITCHEN DESIGN
             </h1>
           </div>
@@ -131,9 +130,12 @@ export default function FirstContent() {
             </button>
           </div>
         </div>
+        </div>
+
+        
       </div>
 
-      <div className="w-full h-full  py-10 pt-10 pb-20 text-white flex flex-col gap-10 justify-center items-center bg-black bg-opacity-90">
+      <div className="w-full h-full  py-10 pt-10 pb-20 text-white flex flex-col gap-10 justify-center items-center bg-black bg-opacity-90 ">
         <div className="w-64">
           <h1 className="font-jost text-3xl font-extrabold uppercase text-center">
             Lorem ipsum dolor sit amet
